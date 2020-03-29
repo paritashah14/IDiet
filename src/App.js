2b2024eaeb2e9587e0f5ee4a1e4f4467e1236c6c
Jack Homan <jhoman@deloitte.com>
2020-03-29 15:36:48 -0400

    filename is fucked up

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Login from './Login';
 import Register from './Register';
 import ForgotPassword from './ForgotPassword';
 import Content from './Content';
-import logo from './iDiet.svg';
+import logo from './idiet.svg';
 
 class App extends Component{
   constructor(props){
